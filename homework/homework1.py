1b1ebd68b8029b03e0ef310628ec8e2c0b56dba5
viktoria-yankova <Viktoriaynk@gmail.com>
2025-02-05 19:33:20 +0200

    initial commit

---
+++ homework/homework1.py
@@ -0,0 +1,7 @@
+first_name = 'Viktoria'
+second_name = 'Hristova'
+last_name = 'Ýankova'
+age = 21
+print('Hello, my name is ' + first_name + ' ' + second_name + ' ' + last_name + ', I am ' + str(age) + ' years old')
+
+
