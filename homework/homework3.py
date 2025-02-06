41aa5b3ffc1e126024c91d739e62a454532bf6af
viktoria-yankova <Viktoriaynk@gmail.com>
2025-02-06 08:36:32 +0200

    excersise

--- homework/homework3.py
+++
@@ -1,8 +0,0 @@
-numbers = input("enter numbers separated by space: ")
-number = [int(num) for num in numbers.split(",")]
-largest = max(number)
-smallest = min(number)
-print("the largest number is: ", largest)
-print("the smallest number is:", smallest)
-if __name__=="__main__":
-    print()
--- homework/hw6.py
+++
@@ -1,8 +0,0 @@
-def num(n):
-    count = 0
-    for i in range(1, n+1):
-        if n%i == 0:
-            count += 1
-    return count
-start= int(input("Enter the start:"))
-end= int (input("Enter the end:"))
